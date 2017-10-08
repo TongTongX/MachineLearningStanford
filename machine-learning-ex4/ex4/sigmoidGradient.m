5fc3d242042141deca259e1ce23325df605a2ce5
TongTong <alanzhaoxutong@hotmail.com>
2017-10-07 23:37:55 -0600

    implemented nnCostFunction, randInitializeWeights, and sigmoidGradient

--- machine-learning-ex4/ex4/sigmoidGradient.m
+++ machine-learning-ex4/ex4/sigmoidGradient.m
@@ -12,7 +12,8 @@ g = zeros(size(z));
 % Instructions: Compute the gradient of the sigmoid function evaluated at
 %               each value of z (z can be a matrix, vector or scalar).
 
-
+a = sigmoid(z);
+g = a .* (1 - a);
 
 
 
